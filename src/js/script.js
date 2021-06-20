2cfbee63943baa8dacf56669f6dc8419e47cc8ad
Gabriel T Viana <gabriel_sp2001@hotmail.com>
2021-06-20 00:40:35 -0300

    alguns ajustes

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Cadastro
 function cadastrar(){
     var username = document.getElementById('username');
     var useremail = document.getElementById('useremail');
@@ -28,6 +29,59 @@ function cadastrar(){
     userpass.value = "";
     userrepeatpass.value = "";
 }
+
+
+
+
+
+// Login
+function logar(){
+    var email = document.getElementById("email");
+    var pass = document.getElementById("pass");
+
+    console.log(email.value+pass.value);
+
+    if (email.value == "teste@teste.com" && pass.value == "teste"){
+        localStorage.setItem("acess", true);
+
+        window.location.href = "index.html";
+
+    }else{
+        alert("usuario ou senha invalidos");
+    }
+}
+
+
+
+
+var logado = false;
+
+if(localStorage.getItem("dadosPessoas") == "true"){
+    logado = true;
+    console.log("entrou");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function cadastrardae(){
