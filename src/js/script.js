c0cc549b68b8470dd9eee8177299ab02a98388f6
Gabriel T Viana <gabriel_sp2001@hotmail.com>
2021-06-20 10:33:21 -0300

    Função Cadastro

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+// Cadastro
+function cadastrar(){
+    var username = document.getElementById('username');
+    var useremail = document.getElementById('useremail');
+    var userpass = document.getElementById('userpass');
+    var userrepeatpass = document.getElementById('userrepeatpass');
+
+    var dados = JSON.parse(localStorage.getItem('dadosPessoas'));
+
+    if(dados == null){
+        localStorage.setItem("dadosPessoas", "[]");
+        dados = [];
+    }
+
+    var auxRegistro = {
+        nome: username.value,
+        email: useremail.value,
+        senha: userpass.value,
+        repetirSenha: userrepeatpass.value
+    }
+
+    dados.push(auxRegistro);
+    
+    localStorage.setItem("dadosPessoas", JSON.stringify(dados));
+    alert("Registro Incluido com sucesso");
+
+    username.value = "";
+    useremail.value = "";
+    userpass.value = "";
+    userrepeatpass.value = "";
+}
