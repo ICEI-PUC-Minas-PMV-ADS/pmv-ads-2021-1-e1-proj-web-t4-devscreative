e9dc2aeafcf25ba98ef7e8c20b15d75b2889f560
Gabriel T Viana <gabriel_sp2001@hotmail.com>
2021-06-20 21:11:06 -0300

    Função Login
    Armazenamento local do Login

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 // Cadastro
-
-function cadastrar(){
+function cadastrar() {
     var username = document.getElementById('username');
     var useremail = document.getElementById('useremail');
     var userpass = document.getElementById('userpass');
@@ -8,7 +7,7 @@ function cadastrar(){
 
     var dados = JSON.parse(localStorage.getItem('dadosPessoas'));
 
-    if(dados == null){
+    if (dados == null) {
         localStorage.setItem("dadosPessoas", "[]");
         dados = [];
     }
@@ -21,7 +20,7 @@ function cadastrar(){
     }
 
     dados.push(auxRegistro);
-    
+
     localStorage.setItem("dadosPessoas", JSON.stringify(dados));
     alert("Registro Incluido com sucesso");
 
@@ -29,4 +28,38 @@ function cadastrar(){
     useremail.value = "";
     userpass.value = "";
     userrepeatpass.value = "";
+}
+
+
+//login 
+const signUp = e => {
+    let formData = {
+        email: document.getElementById("email").value,
+        pass: document.getElementById('pass').value
+    }
+    localStorage.setItem('formData', JSON.stringify(formData));
+
+    dispData();
+    e.preventDefault();
+}
+
+function dispData() {
+    if (localStorage.getItem('formData')) {
+        let { email, pass } = JSON.parse(localStorage.getItem('formData'));
+        var output = document.getElementById('output');
+        output.innerHTML = `
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Endereço de Email</td>
+                            <td>${email}</td>
+                        </tr>
+                        <tr>
+                            <td>Senha</td>
+                            <td>${pass}</td>
+                        </tr>
+                    </tbody>
+               </table>
+            `;
+    }
 }
