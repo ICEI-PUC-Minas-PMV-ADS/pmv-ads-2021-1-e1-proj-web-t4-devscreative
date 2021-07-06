f6b116607794308fd7d5d7d5bee0db334f128887
Milena Lara <milenalarareis@gmail.com>
2021-07-05 22:36:22 -0300

    alterando arquivo js do cadastro

--- src/js/completarCadastro.js
+++ src/js/completarCadastro.js
@@ -1,48 +1,30 @@
-const atualizarCadastro = (evento) => {
-
-    evento.preventDefault();
-
-    const cadastro = JSON.parse(localStorage.getItem("cadastroCompleto")) || [];
-
-    const nomeInput = document.getElementById("username");
-    const nome = nomeInput.value;
-
-    const emailInput = document.getElementById("useremail");
-    const email = emailInput.value;
-
-    const enderecoInput = document.getElementById("userendereco");
-    const endereco = enderecoInput.value;
-
-    const numeroInput = document.getElementById("usernumeroendereco");
-    const numero = numeroInput.value;
-
-    const complementoInput = document.getElementById("usercomplemento");
-    const complemento = complementoInput.value;
-
-    const cepInput = document.getElementById("usercep");
-    const cep = document.cepInput.value;
-
-    const estadoInput = document.getElementById("userestado");
-    const estado = estadoInput.value;
-
-    const cidadeInput = document.getElementById("usercidade");
-    const cidade = cidadeInput.value;
-
-    dados = {
-        nome,
-        email,
-        endereco,
-        numero,
-        complemento,
-        cep,
-        estado,
-        cidade
-    };
-
-    const cadastroAtualizado = [...cadastroCompleto, dados];
-
-    localStorage.setItem("cadastroCompleto", JSON.stringify(cadastroAtualizado));
-}
-
+const inputNome = document.getElementById("nome");
+const inputSobreNome = document.getElementById("sobrenome");
+const inputEmail = document.getElementById("email");
+const inputEndereco = document.getElementById("endereco");
+const inputNumero = document.getElementById("numero");
+const inputComplemento = document.getElementById("complemento");
+const inputCep = document.getElementById("cep");
+// const inputEstado = document.getElementById("");
+// const inputCidade = document.getElementById("");
 const btnEnviar = document.getElementById("btn-enviar");
-btnEnviar.addEventListener("click", atualizarCadastro);
+
+const guardarDados = (() => {
+    let users = JSON.parse(localStorage.getItem("user") || "[]");
+
+    const dados = {
+        "nome": inputNome.value,
+        "sobrenome": inputSobreNome.value,
+        "email": inputEmail.value,
+        "endereco": inputEndereco.value,
+        "numero": inputNumero.value,
+        "complemento": inputComplemento.value,
+        "cep": inputCep.value
+    }
+    users.push(dados);
+
+    localStorage.setItem("user", JSON.stringify(users));
+    // location.reload();
+});
+
+btnEnviar.addEventListener("click", guardarDados);
